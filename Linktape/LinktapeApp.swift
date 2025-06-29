82288de00637a21a4f8f73e8e474612ec599ff68
Daiki Fujimori <G016C1102@outlook.jp>
2025-06-29 19:27:21 +0900

    Initial Commit

---
+++ Linktape/LinktapeApp.swift
@@ -0,0 +1,18 @@
+//
+//  LinktapeApp.swift
+//  Linktape
+//  
+//  Created by Daiki Fujimori on 2025/06/29
+//  
+
+
+import SwiftUI
+
+@main
+struct LinktapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LinktapeTests/LinktapeTests.swift
@@ -0,0 +1,18 @@
+//
+//  LinktapeTests.swift
+//  LinktapeTests
+//  
+//  Created by Daiki Fujimori on 2025/06/29
+//  
+
+
+import Testing
+@testable import Linktape
+
+struct LinktapeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
